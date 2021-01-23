7494981cb0044537681833572d3fd3572e4f3b73
Yigitcan Cakar <yigitcanckr97@gmail.com>
2021-01-23 15:12:41 +0100

    Add project files.

---
+++ FourOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math
+{
+    class FourOperations
+    {
+        public void Add(int Number1, int Number2)
+        {
+            int Add = Number1 + Number2;
+            Console.WriteLine("Result"  + " " +  Add);
+
+        }
+
+        public void Multiplication(int Number1,int Number2)
+        {
+
+            int Multiplication = Number1 * Number2;
+            Console.WriteLine("Result" + " " + Multiplication);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FourOperations fourOperations = new FourOperations();
+
+            fourOperations.Add(2, 9);
+            fourOperations.Add(6, 9);
+            fourOperations.Multiplication(9, 6);
+        }
+    }
+}
